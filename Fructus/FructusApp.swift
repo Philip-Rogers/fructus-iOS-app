cdb0a96eb87c3d223aef686da4f82c64e337f00f
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-01-31 17:17:49 +1000

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Philip Rogers on 31/1/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
