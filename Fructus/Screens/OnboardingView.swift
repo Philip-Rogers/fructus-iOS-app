62c59ef2f14d84cf971e147d744cbcaf57ebf198
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-01 00:39:52 +1000

    initial commit

---
+++ Fructus/Screens/OnboardingView.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingView.swift
+//  Fructus
+//
+//  Created by Philip Rogers on 31/1/22.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+// MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
