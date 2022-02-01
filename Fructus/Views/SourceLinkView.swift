eef3980907682b7f8097cd25f7f0c9d522b92351
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-01 20:34:16 +1000

    init

---
+++ Fructus/Views/SourceLinkView.swift
@@ -0,0 +1,30 @@
+//
+//  SourceLinkView.swift
+//  Fructus
+//
+//  Created by Philip Rogers on 1/2/22.
+//
+
+import SwiftUI
+
+struct SourceLinkView: View {
+    var body: some View {
+        GroupBox() {
+            HStack {
+                Text("Content Source")
+                Spacer()
+                Link("Wikipedia", destination: URL(string: "https://wikipedia.com")!)
+                Image(systemName: "arrow.up.right.square")
+            } //: HSTACK
+            .font(.footnote)
+        }
+    }
+}
+
+struct SourceLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SourceLinkView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
