b196acdfedfce65e339a27c7702c4e900d5b81cd
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-01 14:17:20 +1000

    init

---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,19 @@
+//
+//  FruitModel.swift
+//  Fructus
+//
+//  Created by Philip Rogers on 1/2/22.
+//
+
+import SwiftUI
+
+//: MARK: - FRUITS DATA MODEL
+struct Fruit: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var image: String
+    var gradientColors: [Color]
+    var description: String
+    var nutrition: [String]
+}
